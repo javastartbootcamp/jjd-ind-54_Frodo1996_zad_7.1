dbf3dd9121774efdcea219b47eda2684d939da80
Krzysiek Przystarz <frodson35@gmail.com>
2022-12-27 14:56:35 +0100

    zadanie 7.1

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -1,9 +1,16 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
+
 public class PalindromeService {
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
     public boolean isPalindrome(int[] array) {
-        return false;
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
